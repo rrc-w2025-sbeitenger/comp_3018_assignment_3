4407f1ac7bd9ea445b39f58afe53b3469d2ba366
Sven Beitenger <sbeitenger@academic.rrc.ca>
2026-02-25 23:21:15 -0600

    feat: Created the event create request model.

---
+++ src/api/v1/models/eventCreateRequestModel.ts
@@ -0,0 +1,9 @@
+export interface eventCreateRequest{
+    id: string,
+    name: string,
+    date: string,
+    capacity: number,
+    registrationCount: number,
+    status: string,
+    category: string,
+}
