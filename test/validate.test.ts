9845d52260d179ca2b7307cd49e5453a81854913
Sven Beitenger <sbeitenger@academic.rrc.ca>
2026-02-25 18:29:11 -0600

    feat: Added middleware test to check response body if registrationCount is not an integer.

--- test/validate.test.ts
+++ test/validate.test.ts
@@ -211,4 +211,29 @@ describe("validateRequest Middleware", () => {
     });
     expect(mockNext).not.toHaveBeenCalled();
     });
+
+    it("should fail for registrationCount not being a integer", () => {
+    //Arrange
+    const testSchemas = {
+        body: Joi.object({
+            name: Joi.string().min(3).trim().required(),
+            capacity: Joi.number().integer().min(5).required(),
+            registrationCount: Joi.number().integer().max(Joi.ref("capacity")).optional(),
+            date: Joi.date().greater('now').iso().required(),
+        }),
+    };
+
+    mockReq.body = { name: "counter-strike", capacity: 150, registrationCount: "Joee", date: "2026-12-25T09:00:00.000Z"};
+    const middleware = validateRequest(testSchemas);
+
+    //Act
+    middleware(mockReq as Request, mockRes as Response, mockNext);
+
+    //Assert
+    expect(mockRes.status).toHaveBeenCalledWith(400);
+    expect(mockRes.json).toHaveBeenCalledWith({
+        error: expect.stringContaining("Validation error"),
+    });
+    expect(mockNext).not.toHaveBeenCalled();
+    });
 });
