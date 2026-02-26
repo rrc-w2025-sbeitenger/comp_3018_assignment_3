ae4aa780e001886d1029c574d109fa0b3161045e
Sven Beitenger <sbeitenger@academic.rrc.ca>
2026-02-25 21:04:16 -0600

    feat: Created controller for createEvent that gets the request body.

---
+++ src/api/v1/controllers/eventController.ts
@@ -0,0 +1,26 @@
+import { Request, Response } from "express";
+import { HTTP_STATUS } from "../../../constants/httpConstant";
+import { 
+    HealthCheckResponse,
+     getHealthStatusService,
+      createEventService,
+    } from "../services/itemServices";
+import { successResponse } from "../models/responseModel";
+
+export const getHealthCheck = (req: Request, res: Response): void => {
+    const healthStatus: HealthCheckResponse = getHealthStatusService();
+    res.status(HTTP_STATUS.OK).json(successResponse(healthStatus));
+}
+
+export const createEvent = (req: Request, res:Response): void => {
+    const name: string = req.body.name;
+    //gets checked for ISO date in middleware.
+    const date: string = req.body.date;
+    const capacity: number = Number(req.body.capacity);
+    const registrationCount: number = Number(req.body.registrationCount);
+    const status: string = req.body.status;
+    const category: string = req.body.category;
+
+    const newEvent = createEventService(name, date, capacity, registrationCount, status, category);
+    res.status(HTTP_STATUS.OK).send(newEvent);
+}
