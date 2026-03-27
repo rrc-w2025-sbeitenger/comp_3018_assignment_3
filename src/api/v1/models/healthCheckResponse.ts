ceb40e65419134358952efc4c1f253d365b92a56
Sven Beitenger <sbeitenger@academic.rrc.ca>
2026-03-26 20:31:47 -0500

    feat: Added openapi comment for swagger documention on HealthCheckResponse interface.

--- src/api/v1/models/healthCheckResponse.ts
+++ src/api/v1/models/healthCheckResponse.ts
@@ -1,3 +1,25 @@
+
+/**
+ * @openapi
+ * components:
+ *   schemas:
+ *     HealthCheckResponse:
+ *       type: object
+ *       properties:
+ *         status:
+ *           type: integer
+ *           example: 200
+ *         uptime:
+ *           type: number
+ *           example: 20.5620736
+ *         timestamp:
+ *           type: string
+ *           format: date-time
+ *           example: '2026-03-26T22:33:50.348Z'
+ *         version:
+ *           type: string
+ *           example: '1.0.0'
+ */
 export interface HealthCheckResponse {
     status: number;
     uptime: number;
